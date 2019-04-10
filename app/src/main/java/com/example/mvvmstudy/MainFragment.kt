efffa6f7c3092999b64076aacd9ce1f70aac6fee
Taiga Miyazaki <mk2_tg000@icloud.com>
2019-04-10 17:49:14 +0900

    双方向データバインディングを使った実装

--- app/src/main/java/com/example/mvvmstudy/MainFragment.kt
+++ app/src/main/java/com/example/mvvmstudy/MainFragment.kt
@@ -28,48 +28,51 @@ class MainFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel.liveDataText.observe(this, Observer {
-            binding.countText.text = "文字数：" + it.length.toString()
-        })
+        //ステップ②
+//        viewModel.liveDataText.observe(this, Observer {
+//            binding.countText.text = "文字数：" + it.length.toString()
+//        })
     }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main_binding, container, false)
-        binding.viewModel = viewModel
-        viewModel.fetchText()
-        return binding.root
-    }
-
-
-    // 以下はDataBindingを使わない方法
 
 //    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-//        return inflater.inflate(R.layout.fragment_main, container, false)
+//        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main_binding, container, false)
+//        //ステップ③
+////        binding.lifecycleOwner = viewLifecycleOwner
+//        binding.viewModel = viewModel
+//        viewModel.fetchText()
+//        return binding.root
 //    }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        editText = view.findViewById(R.id.edit_text)
-//        textView = view.findViewById(R.id.count_text)
-//
-//        editText.addTextChangedListener(object : TextWatcher {
-//            override fun afterTextChanged(text: Editable?) {
-//                // Do Nothing
-//            }
-//
-//            override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                //Do Nothing
-//            }
-//
-//            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                textView.text = "文字数：" + text?.length.toString()
-//            }
-//        })
-//
-//        viewModel.fetchText {
-//            editText.setText(it, TextView.BufferType.NORMAL)
-//            textView.text = "文字数：" + it.length.toString()
-//        }
-//    }
+
+    // ステップ①
+    // 以下はDataBindingを使わない方法
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        editText = view.findViewById(R.id.edit_text)
+        textView = view.findViewById(R.id.count_text)
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(text: Editable?) {
+                // Do Nothing
+            }
+
+            override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //Do Nothing
+            }
+
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                textView.text = "文字数：" + text?.length.toString()
+            }
+        })
+
+        viewModel.fetchText {
+            editText.setText(it, TextView.BufferType.NORMAL)
+            textView.text = "文字数：" + it.length.toString()
+        }
+    }
 }
